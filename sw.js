3f029aa815d09b996ec76009dadb81b9558af02e
vyel <violeta_liliana2000@yahoo.com>
2018-12-26 22:53:52 +0200

    Change relative paths

--- sw.js
+++ sw.js
@@ -20,24 +20,24 @@
       caches.open(staticCacheName).then( cache => {
         return cache
           .addAll([
-            "/",
-            "/index.html",
-            "/restaurant.html",
-            "/js/dbhelper.js",
-            "/js/main.js",
-            "/js/restaurant_info.js",
-            "/css/styles.css",
-            "/data/restaurants.json",
-            "/img/1.jpg",
-            "/img/2.jpg",
-            "/img/3.jpg",
-            "/img/4.jpg",
-            "/img/5.jpg",
-            "/img/6.jpg",
-            "/img/7.jpg",
-            "/img/8.jpg",
-            "/img/9.jpg",
-            "/img/10.jpg"
+            "./",
+            "./index.html",
+            "./restaurant.html",
+            "./js/dbhelper.js",
+            "./js/main.js",
+            "./js/restaurant_info.js",
+            "./css/styles.css",
+            "./data/restaurants.json",
+            "./img/1.jpg",
+            "./img/2.jpg",
+            "./img/3.jpg",
+            "./img/4.jpg",
+            "./img/5.jpg",
+            "./img/6.jpg",
+            "./img/7.jpg",
+            "./img/8.jpg",
+            "./img/9.jpg",
+            "./img/10.jpg"
           ])
           .catch( error => {
             console.log("Caches open failed: " + error);
